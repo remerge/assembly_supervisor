7e31dfbd049f15fc7794a3ede2b49d17c1404edd
Cedric Sohrauer <sohrauer@gmail.com>
2019-12-18 12:12:35 +0100

    add tooltips

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,8 +1,13 @@
-import { PodStatus } from "../kube";
+import { PodStatus, Pod } from "../kube";
+import relativeTime from "../relativeTime";
 
 export function statusCssClass(status: PodStatus): string {
   if (status === "Succeeded") return "green";
   if (status === "Pending") return "yellow";
   if (status === "Running") return "blue";
   return "red";
+}
+
+export function tooltipForPod(pod: Pod): string {
+  return `${pod.name} (Created ${relativeTime(pod.createdAt)}): Click to view in GCP`;
 }
